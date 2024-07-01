d0bbabe621862126bded205739ca5fd73fd55a75
Abhinav Chakilam <abhinavchakilam24@gmail.com>
2024-07-01 22:40:05 +0530

    Homepage changes

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -4,9 +4,11 @@ import { styled } from 'baseui';
 import { Button, SHAPE } from 'baseui/button';
 import { Heading, HeadingLevel} from 'baseui/heading';
 import { HeadingXLarge, HeadingMedium, ParagraphLarge } from 'baseui/typography';
-import { Card, StyledBody } from 'baseui/card';
+import { Card, StyledBody, StyledAction, StyledThumbnail } from "baseui/card";
 import { Grid, Cell } from 'baseui/layout-grid';
 import { useNavigate} from "react-router-dom";
+import logo from '../Assets/logo.png';
+import LandingPageImage from '../Assets/doctor-landing-page.png';
 
 
 const Section = styled('div', {
@@ -15,11 +17,18 @@ const Section = styled('div', {
   backgroundColor: '#f5f7fa',
 });
 
-const StatisticsSection = styled('div', {
-  backgroundColor: '#ffffff',
-  padding: '60px 20px',
+const WhySection = styled('div', {
+  padding: '30px 20px',
   textAlign: 'center',
+  backgroundColor: '#f5f7fa',
 });
+
+const TipsSection = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: '18px',
+  padding: '0 80px 80px 80px'
+})
 
 const StyledCard = styled(Card, {
   boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
@@ -48,44 +57,86 @@ const HomePage = () => {
             Get Started
           </Button>
         </div>
-      <Section>
-        <HeadingXLarge>Why Blood Connect?</HeadingXLarge>
-        <ParagraphLarge overrides={{ Block: { style: { textAlign: 'center', padding: '50px', paddingTop: '0px'} } }}>
-          Welcome to Blood Connect, a unique platform dedicated to connecting blood donors with recipients in need. Our mission is to build a compassionate community that saves lives through seamless and efficient blood donations. By providing an easy-to-use, secure system with geo-searching and real-time updates about donors, we strive to make the process of giving and receiving blood as effortless and hassle-free as possible. Join us in our mission to give back to society and make a significant impact, one donation at a time.
-        </ParagraphLarge>
-      </Section>
+      <WhySection>
+        <HeadingXLarge overrides={{ Block: { style: { marginTop: '5px', marginBottom: '8px' } } }}>Why Blood Connect?</HeadingXLarge>
+          <div className='why-blood-connect'>
+            <div className='why-bc-image'>
+              <img src={LandingPageImage} height={450} width={450} alt="An logo of a doctor"/>
+            </div>
+            <div className='why-bc-cards'>
+              <Card title="Unique Platform" overrides={{ Root: { style: { width: "450px" } } }}>
+                <StyledBody>
+                  Connects donors and recipients, fostering a community dedicated to saving lives through efficient blood donations.
+                </StyledBody>
+              </Card>
+              <Card title="User-Friendly and Secure" overrides={{ Root: { style: { width: "450px" } } }}>
+                <StyledBody>
+                  Our application offers geo-searching and real-time updates, ensuring a seamless experience for users.
+                </StyledBody>
+              </Card>
+              <Card title="Mission-Driven Impact" overrides={{ Root: { style: { width: "450px" } } }}>
+                <StyledBody>
+                  Committed to giving back to society, we aim to make the donation process effortless and accessible for all.
+                </StyledBody>
+              </Card>
+            </div>
+          </div>
+      </WhySection>
 
-      <StatisticsSection>
-        <Grid>
-          <Cell span={[4, 8, 12]}>
-            <HeadingXLarge>Our Impact</HeadingXLarge>
-          </Cell>
-          <Cell span={[4, 4, 4]}>
-            <StyledCard>
-              <StyledBody>
-                <HeadingMedium>1000+</HeadingMedium>
-                <ParagraphLarge>Donors Registered</ParagraphLarge>
-              </StyledBody>
-            </StyledCard>
-          </Cell>
-          <Cell span={[4, 4, 4]}>
-            <StyledCard>
-              <StyledBody>
-                <HeadingMedium>500+</HeadingMedium>
-                <ParagraphLarge>Lives Saved</ParagraphLarge>
-              </StyledBody>
-            </StyledCard>
-          </Cell>
-          <Cell span={[4, 4, 4]}>
-            <StyledCard>
-              <StyledBody>
-                <HeadingMedium>300+</HeadingMedium>
-                <ParagraphLarge>Blood Drives Organized</ParagraphLarge>
-              </StyledBody>
-            </StyledCard>
-          </Cell>
-        </Grid>
-      </StatisticsSection>
+      <div>
+      <HeadingXLarge >Tips</HeadingXLarge>
+      <TipsSection>
+      <Card
+      overrides={{ Root: { style: { width: "370px", padding: '10px' } } }}
+      title="Before Donating"
+    >
+      <StyledThumbnail
+        src={logo}
+      />
+      <StyledBody>
+      Ensure you're well-hydrated by drinking plenty of water, eat a healthy meal rich in iron, and avoid fatty foods.
+      </StyledBody>
+      <StyledAction>
+        <Button overrides={{ BaseButton: { style: { width: "40%" } } }} shape={SHAPE.pill}>
+          Read More
+        </Button>
+      </StyledAction>
+    </Card>
+
+    <Card
+      overrides={{ Root: { style: { width: "370px", padding: '10px' } } }}
+      title="During Donation"
+    >
+      <StyledThumbnail
+        src={logo}
+      />
+      <StyledBody>
+      Stay calm and relaxed, and let the staff know if you feel unwell at any point during the process.
+      </StyledBody>
+      <StyledAction>
+        <Button overrides={{ BaseButton: { style: { width: "40%" } } }} shape={SHAPE.pill}>
+          Read More
+        </Button>
+      </StyledAction>
+    </Card>
+    <Card
+      overrides={{ Root: { style: { width: "370px", padding: '10px' } } }}
+      title="After Donating">
+      <StyledThumbnail
+        src={logo}
+      />
+      <StyledBody>
+      Rest for a few minutes, drink extra fluids, and avoid strenuous activities for the rest of the day.
+      </StyledBody>
+      <StyledAction>
+        <Button overrides={{ BaseButton: { style: { width: "40%" } } } } shape={SHAPE.pill}>
+          Read More
+        </Button>
+      </StyledAction>
+    </Card>
+      </TipsSection>
+      </div>
+      
 
       <Section>
         <HeadingXLarge>How It Works</HeadingXLarge>
